a5a6e9e73bb0042eb351175cf2d64afef9e87aae
Danny Libin <dlibinrx@gmail.com>
2020-09-28 20:40:14 -0500

    Create Microsoft.Powershell_profile.ps1

---
+++ Microsoft.Powershell_profile.ps1
@@ -0,0 +1,7 @@
+function prompt {
+  $leafPath = Split-Path $(Get-Location) -Leaf
+  $nl = [Environment]::NewLine
+  $ESC = [char]27
+
+  '[' + $ESC + '[32m' + $env:USERNAME + '@' + $env:COMPUTERNAME + $ESC + '[0m]' + ':[' + $ESC + '[35m' + $leafPath + $ESC + '[0m]' + $nl + $ESC + '[31m$ ' + $ESC + '[0m'
+}
