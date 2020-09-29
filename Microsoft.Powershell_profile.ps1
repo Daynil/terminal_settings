b568c5c935ab192a11996cdee517c9ae214f17f2
Danny Libin <dlibinrx@gmail.com>
2020-09-28 20:52:48 -0500

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -5,3 +5,5 @@ function prompt {
 
   '[' + $ESC + '[32m' + $env:USERNAME + '@' + $env:COMPUTERNAME + $ESC + '[0m]' + ':[' + $ESC + '[35m' + $leafPath + $ESC + '[0m]' + $nl + $ESC + '[31m$ ' + $ESC + '[0m'
 }
+
+Set-Location C:\Coding\
