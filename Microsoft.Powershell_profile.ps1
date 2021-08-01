faf00651c2c6282ce358d0e3895f0c06b457f069
Danny Libin <dlibinrx@gmail.com>
2021-08-01 13:37:32 -0500

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,6 +1,14 @@
 # https://ohmyposh.dev/
 oh-my-posh --init --shell pwsh --config "C:\Users\Danny\AppData\Local\Programs\oh-my-posh\themes\half-life-custom.omp.json" | Invoke-Expression
 
+# shows virtual environment when present
+function Set-EnvVar {
+  if ($env:VIRTUAL_ENV -ne $null) {
+      $env:PVENV = Split-Path $env:VIRTUAL_ENV -Leaf
+  }
+}
+New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global -Force
+
 # OLD
 # function prompt {
 #   $leafPath = Split-Path $(Get-Location) -Leaf
